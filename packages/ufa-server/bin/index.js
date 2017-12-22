e0e24f3195a690cb7358bb4069b53c3ef8b6c1b3
Gurudath Reddy <gurudath.jcreddy@gmail.com>
2017-12-22 10:30:16 +0800

    feat(server): server emulator
    Server to serve static resources and emulate endpoints

---
+++ packages/ufa-server/bin/index.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+const argv = require('yargs')
+    .usage('\nWeb Server to serve static resources and mock api endpoints.\n\n$0 -m <mode> -s <static-content-folder> -a <mock-api-folder> -p <port>')
+    .options({
+        'mode': {
+            type: 'string',
+            alias: 'm',
+            group: 'Server Options:',
+            default: 'both',
+            choices: ['both', 'static', 'api'],
+            description: 'Serve - Static resources or Mock API Endpoints or Both'
+        },
+        'staticpath': {
+            alias: 's',
+            describe: 'Static content relative path',
+            default: './dist',
+            type: 'string',
+            group: 'Server Options:'
+        },
+        'apipath': {
+            alias: 'a',
+            describe: 'Mock api relative path',
+            default: './mock-api',
+            group: 'Server Options:',
+            type: 'string'
+        },
+        'port': {
+            alias: 'p',
+            describe: 'Port number',
+            default: 9000,
+            group: 'Server Options:',
+            type: 'number'
+        }
+    })
+    .help()
+    .version()
+    .strict(true)
+    .argv;
+
+const server = require('./server.js');
+server.start(argv);
+
+
